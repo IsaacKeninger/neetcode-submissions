29ea7ba2dbd1d686b0b09f87ac97a0dbb6a568c6
Isaac Keninger <isaac.keninger@gmail.com>
2026-07-13 09:17:58 -0500

    Add: daily-temperatures - submission-12

---
+++ Data Structures & Algorithms/daily-temperatures/submission-12.py
@@ -0,0 +1,12 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        # decreasing montonic stack
+        stack = []
+        result = [0] * len(temperatures)
+        for idx, temp in enumerate(temperatures):
+            while stack and temp > stack[-1][0]:
+                v_temp, v_idx = stack.pop()
+                result[v_idx] = idx - v_idx
+            stack.append((temp, idx))
+        return result
+        
