9ea9ef1b81d5cce006a09fefb20929449b2083b0
Isaac Keninger <isaac.keninger@gmail.com>
2026-07-24 18:17:42 -0500

    Add: anagram-groups - submission-17

---
+++ Data Structures & Algorithms/anagram-groups/submission-17.py
@@ -0,0 +1,10 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        res = defaultdict(list)
+        for s in strs:
+            fmap = [0] * 26
+            for c in s:
+                pos = ord(c) - ord('a')
+                fmap[pos] += 1
+            res[tuple(fmap)].append(s)
+        return list(res.values())
